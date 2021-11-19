590be8d01776f24413be4567d030b8edcc0edf7e
Alex_Shark <51301615+Alex3000sp@users.noreply.github.com>
2021-11-19 13:24:51 +0300

    Bugs fixed. Ready to check

--- main.py
+++ main.py
@@ -2,14 +2,14 @@
 s1 = input("Enter string s1: ")
 s2 = input("Enter string s2: ")
 equal = s1 == s2
-print(f's1 is: {s1}\n'+
-      f's1 lnegth = {len(s1)}\n'+
-      f's2 is: {s2}\n'+
-      f's2 lnegth = {len(s2)}\n'+
-      f's1 first symbol is: {s1[0]}\n'+
-      f's2 last symbol is: {s2[-1]}\n'+
-      f's1 equals s2 - {equal}\n'+
-      f's2 include s1? - {s2 in s1}\n'+
+print(f's1 is: {s1}\n'
+      f's1 lnegth = {len(s1)}\n'
+      f's2 is: {s2}\n'
+      f's2 lnegth = {len(s2)}\n'
+      f's1 first symbol is: {s1[0]}\n'
+      f's2 last symbol is: {s2[-1]}\n'
+      f's1 equals s2 - {equal}\n'
+      f's2 include s1? - {s2 in s1}\n'
       f's1 include s2? - {s1 in s2}')
 #Task 2
 a = int(input("Enter a: "))
